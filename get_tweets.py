b8cd0e417e006844951ea1c58f7e91c7df524df6
Dakini <dakini.mallam-garba@insa-rennes.fr>
2021-10-10 12:55:07 +0200

    Created DataBase to save the tweets

--- get_tweets.py
+++ get_tweets.py
@@ -1,12 +1,34 @@
 import tweepy
+import json
+import pyodbc 
 
-print("Enter consumer Key")
-consumer_key = input()
-print("Enter consumer Secret")
-consumer_secret = input()
+with open("twitter_token.json.txt") as f:
+   tokens = json.load(f)
+
+consumer_key = tokens["Key"]
+consumer_secret = tokens["Secret"]
 
 auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
 
 api = tweepy.API(auth)
-for tweet in tweepy.Cursor(api.search_tweets,lang = "en", q='facebook').items(1):
+
+db_connexion = pyodbc.connect('Driver={SQL Server};'
+                      'Server=localhost;'
+                      'Database=tweets;'
+                      'Trusted_Connection=yes;')
+
+"""
+cursor = db_connexion.cursor()
+cursor.execute('SELECT * FROM table_name')
+
+for i in cursor:
+    print(i)
+"""
+for tweet in tweepy.Cursor(api.search_tweets,lang = "en", q='apple').items(1):
+    print(tweet.created_at)
+    print(tweet.id)
     print(tweet.text)
+    print(tweet.entities["hashtags"])
+    print(tweet.user.time_zone)
+    print(tweet.retweet_count)
+    print(tweet.favorite_count)
