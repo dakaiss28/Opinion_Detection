16d8a220357f0c07f35be20f7824b827559f6ac7
Dakini <dakini.mallam-garba@insa-rennes.fr>
2021-10-10 15:04:11 +0200

    Added method to labelise tweets with nltk + txt2vc

--- get_tweets.py
+++ get_tweets.py
@@ -6,10 +6,11 @@ import re, string
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 stop_words = stopwords.words('english')
 lemmatizer = WordNetLemmatizer()
+dico = {}
 
 def set_up(file):
     with open(file) as f:
@@ -60,18 +61,38 @@ def lemmatize_text(text):
     text = [lemmatizer.lemmatize(t, 'v') for t in text]
     return text
 
-#def score_text(text):
-    
-#def text_to_vector(text):
+def word_int(word):
+    if word not in dico:
+        id = len(dico) + 1
+        dico[word] = id
+    return dico[word]
 
+
+def label_text(text):
+    analyser = SentimentIntensityAnalyzer()
+    res = analyser.polarity_scores(text)
+    neg = res['neg']
+    pos = res['pos']
+    neu = res['neu']
+    if (neg > pos and neg > neu):
+        return -1
+    elif(pos >neg and pos > neu):
+        return 1
+    else:
+        return 0
+    
+def text_to_vector(text):
+    return list(map(word_int,text))
 
 def main():
     (api,db_connexion) = set_up("twitter_token.json.txt")
     for tweet in tweepy.Cursor(api.search_tweets,lang = "en", q='apple').items(2):
         print(tweet.text)
+        label_text(tweet.text)
         clean = clean_text(tweet.text)
-        print(clean)
-        print(lemmatize_text(clean))
+        text = lemmatize_text(clean)
+        print(label_text(tweet.text))
+        print(text_to_vector(text))
         """
         print(tweet.created_at)
         print(tweet.id)
